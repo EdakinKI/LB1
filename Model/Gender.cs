4849bd1ff039bd6101a72ce5a96097cd4c9788b5
EdakinKI <edakin.kostia242@gmail.com>
2025-02-20 14:21:30 +0700

    fix(Lab1): удален Gender

---
+++ Model/Gender.cs
@@ -0,0 +1,18 @@
+namespace Model
+{
+    /// <summary>
+    /// Variants of person's gender.
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Male gender.
+        /// </summary>
+        Male,
+
+        /// <summary>
+        /// Female gender.
+        /// </summary>
+        Female
+    }
+}
