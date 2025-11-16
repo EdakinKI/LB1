9f8ddd121d1c4652343d0372605fcbbca5b718a2
EdakinKI <edakin.kostia242@gmail.com>
2025-11-16 14:16:03 +0700

    feature(LB4): добавлен интерфейс

--- Model/BenchPress.cs
+++ Model/BenchPress.cs
@@ -47,7 +47,6 @@ namespace Model
         public override string ExerciseInfo => $"Жим штанги: {Name}," +
                         $" Вес: {Weight} кг, Повторения: {Repetitions}";
 
-        //TODO: XML+
         /// <summary>
         /// Создание упражнения Жим штанги
         /// </summary>
